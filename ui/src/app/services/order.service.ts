27ffdf4e65b6c36ffc9e1b47483d3a2f7a2f287e
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2026-05-10 15:12:02 -0300

    Decrease retry delay to 1 second.

--- ui/src/app/services/order.service.ts
+++ ui/src/app/services/order.service.ts
@@ -50,7 +50,7 @@ export class OrderService {
         }).pipe(
             bufferTime(500),  // backpressure – matches your Reactor limitRate
             mergeAll(),
-            retry({ delay: 2000 }),
+            retry({ delay: 1000 }),
             share() // one SSE connection for all subscribers
         );
     }
